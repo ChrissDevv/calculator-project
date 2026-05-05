bc019502c486dad01ed45856d7a768a0e92f7f74
Chris <chrissdev.io@gmail.com>
2026-05-05 15:03:29 -0700

    Created a function that holds a value of the number the user clicked and shows it on screen

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
- let firstNum = '';
+let firstNum = '';
  let secondNum = '';
  let theOperator = ''
+// Will select all divs with the class of digit , and run a loop through them and add a eventlistner that waits for a click then runs a function that checks if its the first number clicked or second number click,which then displays it in the display div
+let onlyDigits = document.querySelectorAll('.digit')
+ for( let i = 0; i < onlyDigits.length; i++){
+    onlyDigits[i].addEventListener('click',function (e){
+        if (theOperator === ''){
+            firstNum += e.target.textContent
+            document.querySelector('#display').textContent = firstNum
+        }else {
+            secondNum += e.target.textContent
+            document.querySelector('#display').textContent = secondNum
+        }
+    })
+    
+ }
+
+
+
 function add(a,b){
    return a + b
 }
@@ -18,6 +35,8 @@ function divide(a,b){
    }
 }
 
+
+
 function operate(operater,num1,num2){
     if( operater === '+'){
         return add(num1,num2)
@@ -32,6 +51,4 @@ function operate(operater,num1,num2){
     }
 
 }
-function addone(){
 
-}
